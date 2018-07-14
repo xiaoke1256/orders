83b2a006175fce897d3c9d09e0f1cf333d8a33c8
tangjun <tangjun@CN-20170216CTDG>
2018-07-14 09:21:39 +0800

    remove useless import

--- orders/src/main/java/com/xiaoke1256/orders/vo/SubOrder.java
+++ orders/src/main/java/com/xiaoke1256/orders/vo/SubOrder.java
@@ -2,16 +2,6 @@ package com.xiaoke1256.orders.vo;
 
 import java.math.BigDecimal;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-
-import org.hibernate.annotations.GenericGenerator;
-
 public class SubOrder {
 
 	private Long subOrderId;
