1f3273208290464b07841408307d68ccf92543aa
tangjun <tangjun@CN-20170216CTDG>
2018-07-22 10:35:55 +0800

    index page can access!

--- orders/src/main/java/com/xiaoke1256/orders/SpringbootApplication.java
+++ orders/src/main/java/com/xiaoke1256/orders/SpringbootApplication.java
@@ -1,9 +1,13 @@
 package com.xiaoke1256.orders;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+@SpringBootApplication
 public class SpringbootApplication extends SpringBootServletInitializer {
 	
 	@Override
