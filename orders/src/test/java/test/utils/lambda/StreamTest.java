5f9aafc760888e4b9ba351d9ce1a1fa9c5b9860e
tangjun <tangjun@CN-20170216CTDG>
2018-07-29 12:41:53 +0800

    streamTest

--- orders/src/test/java/test/utils/lambda/StreamTest.java
+++ orders/src/test/java/test/utils/lambda/StreamTest.java
@@ -1,5 +1,41 @@
 package test.utils.lambda;
 
-public class StreamTest {
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
+import org.junit.Test;
+
+public class StreamTest {
+	@Test
+	public void testAddUp() {
+		int sum = addUp(Stream.of(1,2,3,4,5,6));
+		System.out.println("sum is :"+sum);
+	}
+	
+	@Test
+	public void testLowCharCount(){
+		long count = lowCharCout("sadRWE32sdfTew");
+		System.out.println("count is :"+count);
+	}
+	
+	@Test
+	public void testGetMostLowCharsString() {
+		Optional<String> op = getMostLowCharsString(Arrays.asList("ADSWEWEW","DDDDDwDD"));
+		System.out.println("The longest String is :"+op.get());
+	}
+	
+	private int addUp(Stream<Integer> s) {
+		return s.reduce(0, (x,y)-> x+y).intValue();
+	}
+	
+	private long lowCharCout(String s) {
+		return s.chars().filter(c -> c>='a'&&c<='z' ).count();
+	}
+	
+	private Optional<String> getMostLowCharsString(List<String> list){
+		return list.stream().max(Comparator.comparing(s -> lowCharCout(s)));
+	}
 }
