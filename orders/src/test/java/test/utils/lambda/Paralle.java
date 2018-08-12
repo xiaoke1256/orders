3b30d2e550fc547c764423943407a6665c24c6c9
tangjun <tangjun@CN-20170216CTDG>
2018-08-12 16:38:18 +0800

    Paralle

--- orders/src/test/java/test/utils/lambda/Paralle.java
+++ orders/src/test/java/test/utils/lambda/Paralle.java
@@ -2,7 +2,6 @@ package test.utils.lambda;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
