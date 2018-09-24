557468f395cb8f004d80d79f014fc7dcd0d16de9
tangjun <tangjun@CN-20170216CTDG>
2018-09-24 21:50:41 +0800

    add properties to spring config.

--- search/src/main/java/com/xiaoke1256/orders/search/SpringbootApplication.java
+++ search/src/main/java/com/xiaoke1256/orders/search/SpringbootApplication.java
@@ -1,11 +1,22 @@
 package com.xiaoke1256.orders.search;
 
+import java.util.Properties;
+
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
 public class SpringbootApplication {
-	
-	
+	@Bean
+	public PropertiesFactoryBean prop() {
+		PropertiesFactoryBean prop = new PropertiesFactoryBean();
+		Properties properties = new Properties();
+		properties.put("locations", new String[]{"classpath:public.properties"});
+		prop.setProperties(properties );
+		return prop;
+	}
+
 }
