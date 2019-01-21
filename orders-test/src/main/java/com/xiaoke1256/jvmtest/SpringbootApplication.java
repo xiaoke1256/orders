70b345793ed97a3dd2cd8b282885b76e8a28a6ac
tangjun <tangjun@CN-20170216CTDG>
2019-01-21 21:41:01 +0800

    test codes.

---
+++ orders-test/src/main/java/com/xiaoke1256/jvmtest/SpringbootApplication.java
@@ -0,0 +1,21 @@
+package com.xiaoke1256.jvmtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringbootApplication extends SpringBootServletInitializer {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringbootApplication.class);
+    }
+	
+	public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+}
---
+++ orders-test/src/main/java/com/xiaoke1256/jvmtest/controller/ThreadLockController.java
@@ -0,0 +1,64 @@
+package com.xiaoke1256.jvmtest.controller;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.xiaoke1256.common.utils.ResponseUtils;
+
+@RestController
+@RequestMapping("/deadLock")
+public class ThreadLockController {
+	private final ReentrantLock lock1 = new ReentrantLock();
+	
+	private final ReentrantLock lock2 = new ReentrantLock();
+	
+	
+	@RequestMapping(value="/bussieness1")
+	public void bussieness1(HttpServletResponse response) {
+		try {
+			lock1.lock();
+			//do something.
+			Thread.sleep(100);
+			lock2.lock();
+			//do something.
+			Thread.sleep(500);
+			writeToResponse(response,"success!");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			writeToResponse(response,"error!");
+		}finally {
+			lock2.unlock();
+			lock1.unlock();
+		}
+		
+	}
+	
+	@RequestMapping(value="/bussieness2")
+	public void bussieness2(HttpServletResponse response) {
+		try {
+			lock2.lock();
+			//do something.
+			Thread.sleep(100);
+			lock1.lock();
+			//do something.
+			Thread.sleep(500);
+			writeToResponse(response,"success!");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			writeToResponse(response,"error!");
+		}finally {
+			lock1.unlock();
+			lock2.unlock();
+		}
+		
+	}
+	
+	private void writeToResponse(HttpServletResponse response,String s) {
+		ResponseUtils.writeToResponse(response, s);
+	}
+}
