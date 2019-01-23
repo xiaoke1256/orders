f41828ade6a9668b3120cbfe88d35b79c694103d
tangjun <tangjun@CN-20170216CTDG>
2019-01-23 12:21:28 +0800

    redesign db.

--- orders/src/main/sql/create_tables.sql
+++ orders/src/main/sql/create_tables.sql
@@ -5,7 +5,7 @@ drop table O_STORAGE;
 -- Create table
 create table O_STORAGE
 (
-  storage_id	BIGINT primary key not null auto_increment,
+  storage_id	BIGINT primary key not null auto_increment comment '库存主键',
   product_code    CHAR(10) not null comment '商品编号',
   option_code	VARCHAR(12) comment '附加选项编码',
   stock_num     DECIMAL(19) comment '库存量',
@@ -15,13 +15,12 @@ create table O_STORAGE
 
 create table PAY_ORDER
 (
-  pay_order_id BIGINT primary key not null auto_increment,
-  pay_order_no VARCHAR(22) not null,
-  carriage_amt DECIMAL(19,2),
-  payer_no     VARCHAR(24),
-  total_amt    DECIMAL(19,2),
-  insert_time TIMESTAMP not null DEFAULT NOW(),
-  update_time TIMESTAMP not null DEFAULT NOW()
+  pay_order_no VARCHAR(22) primary key not null comment '订单号，主键',
+  carriage_amt DECIMAL(20) comment '运费',
+  payer_no     VARCHAR(24) comment '付款人',
+  total_amt    DECIMAL(22) comment '订单总额（含运费）',
+  insert_time TIMESTAMP not null DEFAULT NOW() comment '插入时间',
+  update_time TIMESTAMP not null DEFAULT NOW() comment '修改时间'
 ) comment='支付单表';
 
   
@@ -29,18 +28,18 @@ create table PAY_ORDER
 
 create table SUB_ORDER
 (
-  sub_order_id  BIGINT primary key not null auto_increment,
-  product_num   DECIMAL(10),
-  product_price DECIMAL(19),
-  store_no      VARCHAR(32) not null,
-  pay_order_id  BIGINT,
-  product_code    CHAR(10)
-);
+   sub_order_id  BIGINT primary key not null auto_increment comment '子订单主键',
+   pay_order_no  BIGINT comment '支付单号',
+   carriage_amt DECIMAL(20)comment '运费',
+   total_amt    DECIMAL(22) comment '订单总额（含运费）',
+   store_no      VARCHAR(32) not null comment '店铺号'
+  
+)comment='子订单表';
 -- Create/Recreate primary, unique and foreign key constraints 
 
 --create index
-CREATE INDEX IDX_PRODUCT_STORE_NO ON PRODUCT(STORE_NO);
+CREATE INDEX IDX_STORE_PRODUCT_NO ON O_STORAGE(product_code);
 CREATE INDEX IDX_PAY_ORDER_PAYER_NO ON PAY_ORDER(PAYER_NO);
-CREATE INDEX IDX_SUB_ORDER_ORDER_ID ON SUB_ORDER (PAY_ORDER_ID);
+CREATE INDEX IDX_SUB_ORDER_ORDER_NO ON SUB_ORDER (PAY_ORDER_NO);
 --CREATE INDEX IDX_SUB_ORDER_STORE_NO ON SUB_ORDER (STORE_NO);
 --CREATE INDEX IDX_SUB_ORDER_PRODUCT_ID ON SUB_ORDER(PRODUCT_ID);
--- product/sql/20180711_add_in_sec_kill.sql
+++ product/sql/20180711_add_in_sec_kill.sql
@@ -1,1 +1,2 @@
+--其实本字段应该放在营销模块中的
 alter table PRODUCT add IN_SECKILL CHAR(1) not null default '0' comment '是否正在进行秒杀活动。（是:1;否:0）';
