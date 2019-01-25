714389b0920a1b8803072ec30d34d0fdcd652b42
tangjun <tangjun@CN-20170216CTDG>
2019-01-25 13:53:39 +0800

    first page

---
+++ product/src/main/java/com/xiaoke1256/orders/product/SpringbootApplication.java
@@ -0,0 +1,19 @@
+package com.xiaoke1256.orders.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringbootApplication extends SpringBootServletInitializer {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringbootApplication.class);
+    }
+	
+	public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+}
