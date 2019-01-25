de9b6a29f467ec35ce9a3f17c3f401be67df14b5
tangjun <tangjun@CN-20170216CTDG>
2019-01-25 15:49:33 +0800

    search by condition.

---
+++ product/src/main/java/com/xiaoke1256/orders/product/service/ProductService.java
@@ -0,0 +1,15 @@
+package com.xiaoke1256.orders.product.service;
+
+import org.springframework.stereotype.Service;
+
+import com.xiaoke1256.orders.common.page.QueryResult;
+import com.xiaoke1256.orders.product.bo.Product;
+import com.xiaoke1256.orders.product.vo.ProductCondition;
+
+@Service
+public class ProductService {
+	
+	public QueryResult<Product> findProductByCondition(ProductCondition condition){
+		return null;
+	}
+}
---
+++ product/src/main/java/com/xiaoke1256/orders/product/vo/ProductCondition.java
@@ -0,0 +1,12 @@
+package com.xiaoke1256.orders.product.vo;
+
+import com.xiaoke1256.orders.common.page.BaseCondition;
+
+public class ProductCondition extends BaseCondition {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
