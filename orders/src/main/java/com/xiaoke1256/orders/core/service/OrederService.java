bd852779137e0cb5994a6777c172136bda313877
tangjun <tangjun@CN-20170216CTDG>
2019-01-29 18:16:52 +0800

    building orders.

--- orders/src/main/java/com/xiaoke1256/orders/core/service/OrederService.java
+++ orders/src/main/java/com/xiaoke1256/orders/core/service/OrederService.java
@@ -1,12 +1,18 @@
 package com.xiaoke1256.orders.core.service;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.UUID;
+import java.util.stream.IntStream;
 
 import javax.persistence.EntityManager;
 import javax.persistence.LockModeType;
@@ -18,10 +24,12 @@ import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 
 import com.xiaoke1256.orders.core.bo.OStorage;
 import com.xiaoke1256.orders.core.bo.PayOrder;
 import com.xiaoke1256.orders.core.bo.SubOrder;
+import com.xiaoke1256.orders.product.dto.Product;
 
 @Service
 @Transactional
@@ -31,24 +39,56 @@ public class OrederService {
 //	private BaseDao baseDao;
 	@PersistenceContext(unitName="default")
 	private EntityManager entityManager ;
+	
 	@Autowired
 	private NoTransOrederService noTransOrederService;
 	
-	public PayOrder place(String payerNo,Map<String,Integer> productMap){
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	public PayOrder place(String payerNo,Map<String,Integer> orderMap){
 		logger.info("start place a oreder.");
 		//productMap = new TreeMap<String,Integer>(productMap);
-		if(productMap.isEmpty())
+		if(orderMap.isEmpty())
 			throw new RuntimeException("empty sub order.");
 			
 		//先查各个商品的信息 ，判断是否是上线商品。
+		List<Product> products = new ArrayList<Product>();
+		for(Map.Entry<String,Integer> enty:orderMap.entrySet()) {
+			String productCode = enty.getKey();
+			Product product = restTemplate.getForObject("http:127.0.0.1/product/product/"+productCode, Product.class);
+			if(!"1".equals(product.getProductStatus())) {
+				throw new RuntimeException("商品未上线。");
+			}
+			products.add(product);
+		}
 		
-		//检查各个商品的运费定价。
+		//按店铺分组。
+		Map<String, List<Product>> groupByStore = groupByStoreNo(products);
+		
+		//检查各个商品的运费定价。（同一店铺商品不超过1公斤的情况下，运费不会叠加）。
+		for(Entry<String, List<Product>> entry:groupByStore.entrySet()) {
+			String storeNo =entry.getKey();
+			List<Product> subProducts = entry.getValue();
+			Map<Product,Integer> subOrderMap = new HashMap<Product,Integer>();
+			for(Product product:subProducts) {
+				subOrderMap.put(product, orderMap.get(product.getProductCode()));
+			}
+			BigDecimal carriage = calCarriage(storeNo,subOrderMap);
+			//构造子订单
+			SubOrder subOrder = new SubOrder();
+			subOrder.setCarriageAmt(carriage);
+			subOrder.setStoreNo(storeNo);
+			//构造订单项
+			
+		}
+		
 		
 		int index = 0;
-		for(Entry<String,Integer> enty:productMap.entrySet()){
+		for(Entry<String,Integer> enty:orderMap.entrySet()){
 			String productId = enty.getKey();
 			Integer num = enty.getValue();
-			if(index>=productMap.size()-1){
+			if(index>=orderMap.size()-1){
 				//boolean success = noTransOrederService.modiyProductStore(productId, num);
 				boolean success = noTransOrederService.modiyProductStoreWithTrans(productId, num);
 				if(!success)
@@ -64,7 +104,7 @@ public class OrederService {
 			index++;
 		}
 		entityManager.flush();
-		PayOrder payOrder = createPayOrder(payerNo, productMap);
+		PayOrder payOrder = createPayOrder(payerNo, orderMap);
 		entityManager.persist(payOrder);
 		entityManager.flush();
 		return payOrder;
@@ -101,6 +141,42 @@ public class OrederService {
 		if(order!=null)
 			Hibernate.initialize(order.getSubOrders());
 		return order;
+	}
+	
+	/**
+	 * 按店铺分组
+	 * @return 其key是店铺号。
+	 */
+	private Map<String,List<Product>> groupByStoreNo(List<Product> products){
+		Map<String,List<Product>> resultMap = new LinkedHashMap<String,List<Product>>();
+		for(Product product:products) {
+			String productCode = product.getProductCode();
+			List<Product> list = resultMap.get(productCode);
+			if(list==null) {
+				list = new ArrayList<Product>();
+				resultMap.put(product.getStore().getStoreNo(), list);
+			}
+			list.add(product);
+		}
+		return resultMap;
+	}
+	
+	/**
+	 * 计算运费
+	 * @param storeNo
+	 * @param products
+	 * @return
+	 */
+	private BigDecimal calCarriage(String storeNo, Map<Product,Integer> subOrderMap) {
+		//算法先简单一点。
+		//5件商品以内运费7元，5件以上按比例增加
+		int productNum = subOrderMap.values().stream().mapToInt(x->x).sum();
+		if(productNum<5) {
+			return new BigDecimal(7000);
+		}else {
+			return new BigDecimal(7000).multiply(BigDecimal.valueOf(productNum)).divide(BigDecimal.valueOf(5), 3, RoundingMode.HALF_UP);
+		}
+		
 	}
 	
 }
--- product/src/main/java/com/xiaoke1256/orders/product/controller/ProductController.java
+++ product/src/main/java/com/xiaoke1256/orders/product/controller/ProductController.java
@@ -1,11 +1,12 @@
 package com.xiaoke1256.orders.product.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.xiaoke1256.orders.product.bo.Product;
+import com.xiaoke1256.orders.product.dto.Product;
 import com.xiaoke1256.orders.product.service.ProductService;
 
 @RestController
@@ -15,6 +16,11 @@ public class ProductController {
 	
 	@GetMapping("/product/{productCode}")
 	public Product getProductByCode(@PathVariable String productCode) {
-		return productService.getProductByCode(productCode);
+		com.xiaoke1256.orders.product.bo.Product product = productService.getProductByCode(productCode);
+		if(product==null)
+			return null;
+		Product dto = new Product();
+		BeanUtils.copyProperties(product, dto);
+		return dto;
 	}
 }
