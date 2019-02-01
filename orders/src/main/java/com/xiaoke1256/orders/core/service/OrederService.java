ac26532a5f44452a5121200e91fa753f820f3436
tangjun <tangjun@CN-20170216CTDG>
2019-02-01 10:58:20 +0800

    place order.

--- orders/src/main/java/com/xiaoke1256/orders/core/service/OrederService.java
+++ orders/src/main/java/com/xiaoke1256/orders/core/service/OrederService.java
@@ -7,15 +7,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
-import java.util.UUID;
-import java.util.stream.IntStream;
 
 import javax.persistence.EntityManager;
-import javax.persistence.LockModeType;
 import javax.persistence.PersistenceContext;
 
 import org.apache.logging.log4j.LogManager;
@@ -27,6 +25,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 import com.xiaoke1256.orders.core.bo.OStorage;
+import com.xiaoke1256.orders.core.bo.OrderItem;
 import com.xiaoke1256.orders.core.bo.PayOrder;
 import com.xiaoke1256.orders.core.bo.SubOrder;
 import com.xiaoke1256.orders.product.dto.Product;
@@ -41,22 +40,25 @@ public class OrederService {
 	private EntityManager entityManager ;
 	
 	@Autowired
-	private NoTransOrederService noTransOrederService;
-	
-	@Autowired
 	private RestTemplate restTemplate;
 	
 	public PayOrder place(String payerNo,Map<String,Integer> orderMap){
 		logger.info("start place a oreder.");
-		//productMap = new TreeMap<String,Integer>(productMap);
 		if(orderMap.isEmpty())
 			throw new RuntimeException("empty sub order.");
+		
+		//商品数量必须大于0
+		for(Integer num:orderMap.values()) {
+			if(num<=0) {
+				throw new RuntimeException("product number must more than zero!");
+			}
+		}
 			
 		//先查各个商品的信息 ，判断是否是上线商品。
 		List<Product> products = new ArrayList<Product>();
 		for(Map.Entry<String,Integer> enty:orderMap.entrySet()) {
 			String productCode = enty.getKey();
-			Product product = restTemplate.getForObject("http:127.0.0.1/product/product/"+productCode, Product.class);
+			Product product = restTemplate.getForObject("http:127.0.0.1:8081/product/product/"+productCode, Product.class);
 			if(!"1".equals(product.getProductStatus())) {
 				throw new RuntimeException("商品未上线。");
 			}
@@ -66,7 +68,8 @@ public class OrederService {
 		//按店铺分组。
 		Map<String, List<Product>> groupByStore = groupByStoreNo(products);
 		
-		//检查各个商品的运费定价。（同一店铺商品不超过1公斤的情况下，运费不会叠加）。
+		//构造子订单
+		List<SubOrder> subOrders = new ArrayList<SubOrder>();
 		for(Entry<String, List<Product>> entry:groupByStore.entrySet()) {
 			String storeNo =entry.getKey();
 			List<Product> subProducts = entry.getValue();
@@ -74,37 +77,41 @@ public class OrederService {
 			for(Product product:subProducts) {
 				subOrderMap.put(product, orderMap.get(product.getProductCode()));
 			}
+			//检查各个商品的运费定价。（同一店铺商品不超过1公斤的情况下，运费不会叠加）。
 			BigDecimal carriage = calCarriage(storeNo,subOrderMap);
 			//构造子订单
 			SubOrder subOrder = new SubOrder();
 			subOrder.setCarriageAmt(carriage);
 			subOrder.setStoreNo(storeNo);
 			//构造订单项
-			
+			Set<OrderItem> items = new HashSet<OrderItem>();
+			for(Product product:subProducts) {
+				OrderItem orderItem = new OrderItem();
+				orderItem.setProductCode(product.getProductCode());
+				orderItem.setProductNum(orderMap.get(product.getProductCode()));
+				orderItem.setSubOrder(subOrder);
+			}
+			//subOrder.setTotalAmt(totalAmt);
+			subOrder.setOrderItems(items);
+			subOrders.add(subOrder);
 		}
 		
-		
-		int index = 0;
+		//扣减库存
 		for(Entry<String,Integer> enty:orderMap.entrySet()){
 			String productId = enty.getKey();
 			Integer num = enty.getValue();
-			if(index>=orderMap.size()-1){
-				//boolean success = noTransOrederService.modiyProductStore(productId, num);
-				boolean success = noTransOrederService.modiyProductStoreWithTrans(productId, num);
-				if(!success)
-					throw new RuntimeException("库存不足");
-			}else{
-				OStorage p = entityManager.find(OStorage.class, productId,LockModeType.PESSIMISTIC_WRITE);
-				if(p.getStockNum()<num){
-					throw new RuntimeException("库存不足");
-				}
-				p.setStockNum(p.getStockNum()-num);
-				entityManager.merge(p);
-			}
-			index++;
+			String hql = "update OStorage set stockNum = (stockNum- ?) where productCode = ? and stockNum>= ? ";
+			int excResult = entityManager.createQuery(hql)
+					.setParameter(1, num.longValue())
+					.setParameter(2, productId)
+					.setParameter(3, num.longValue())
+					.executeUpdate();
+			if(excResult<=0)
+				throw new RuntimeException("库存不足");
 		}
 		entityManager.flush();
-		PayOrder payOrder = createPayOrder(payerNo, orderMap);
+		//创建支付单
+		PayOrder payOrder = createPayOrder(payerNo, subOrders);
 		entityManager.persist(payOrder);
 		entityManager.flush();
 		return payOrder;
@@ -113,25 +120,19 @@ public class OrederService {
 //		}
 	}
 	
-	private PayOrder createPayOrder(String payerNo,Map<String,Integer> productMap){
+	private PayOrder createPayOrder(String payerNo,List<SubOrder> subOrders){
 		PayOrder payOrder = new PayOrder();
 		payOrder.setPayerNo(payerNo);
-		//payOrder.setCarriageAmt(carriageAmt);
-		//payOrder.setPayOrderNo(UUID.randomUUID().toString().substring(0, 22));
+		//计算订单总额
+		BigDecimal totalAmt = BigDecimal.ZERO;
+		for(SubOrder subOrder:subOrders) {
+			totalAmt = totalAmt.add(subOrder.getTotalAmt());
+		}
+		payOrder.setTotalAmt(totalAmt );
 		payOrder.setInsertTime(new Timestamp(System.currentTimeMillis()));
 		payOrder.setUpdateTime(new Timestamp(System.currentTimeMillis()));
-		Set<SubOrder> suborderSet = new HashSet<SubOrder>();
-		for(Entry<String,Integer> entry:productMap.entrySet()){
-			SubOrder subOrder = new SubOrder();
-			subOrder.setPayOrder(payOrder);
-			OStorage p = entityManager.find(OStorage.class,entry.getKey());
-			//subOrder.setProduct(p);
-			//subOrder.setProductNum(entry.getValue());
-			//subOrder.setProductPrice(p.getProductPrice());
-			//subOrder.setStoreNo(p.getStoreNo());
-			suborderSet.add(subOrder);
-		}
-		payOrder.setSubOrders(suborderSet);
+		
+		payOrder.setSubOrders(new LinkedHashSet<SubOrder>(subOrders));
 		return payOrder;
 	}
 	
