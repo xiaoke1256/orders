92dcab6f19fd29f63adbc6e5e396ffb792702431
tangjun <tangjun@CN-20170216CTDG>
2019-03-17 11:19:15 +0800

    common exceptions.

--- common/src/main/java/com/xiaoke1256/orders/common/RespMsg.java
+++ common/src/main/java/com/xiaoke1256/orders/common/RespMsg.java
@@ -1,20 +1,46 @@
 package com.xiaoke1256.orders.common;
 
+import com.xiaoke1256.orders.common.exception.AppException;
+import com.xiaoke1256.orders.common.exception.BusinessException;
+import com.xiaoke1256.orders.common.exception.ErrorCode;
+
 /**
  * Response message for restful request.
  * @author Administrator
  *
  */
 public class RespMsg implements java.io.Serializable {
+	/* constructions*/
 	public RespMsg() {
 		super();
 	}
+	
 	public RespMsg(String code, String msg) {
 		super();
 		this.code = code;
 		this.msg = msg;
 	}
 	
+	public RespMsg(ErrorCode errorCode) {
+		super();
+		this.code = errorCode.getCode();
+		this.msg = errorCode.getMsg();
+	}
+	
+	public RespMsg(AppException ex) {
+		super();
+		this.code = ex.getErrorCode();
+		if(ex instanceof BusinessException)
+			this.msg = ((BusinessException)ex).getShowMsg();
+		else
+			this.msg = ex.getErrorMsg();
+	}
+	
+	public RespMsg(BusinessException ex) {
+		super();
+		this.code = ex.getErrorCode();
+		this.msg = ex.getShowMsg();
+	}
 
 	/**
 	 * 
@@ -35,5 +61,6 @@ public class RespMsg implements java.io.Serializable {
 		this.msg = msg;
 	}
 	
+	public static final RespMsg SUCCESS = new RespMsg(ErrorCode.SUCCESS);
 	
 }
