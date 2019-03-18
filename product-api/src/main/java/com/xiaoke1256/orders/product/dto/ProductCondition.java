646d3201c9d65e84dc167c9d7455e211586ca284
tangjun <tangjun@CN-20170216CTDG>
2019-03-18 18:42:00 +0800

    product api

---
+++ product-api/src/main/java/com/xiaoke1256/orders/product/dto/ProductCondition.java
@@ -0,0 +1,54 @@
+package com.xiaoke1256.orders.product.dto;
+
+import com.xiaoke1256.orders.common.page.BaseCondition;
+
+public class ProductCondition extends BaseCondition {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String productCode;
+	
+	private String productName;
+	
+	private String productTypeName;
+	
+	private boolean needFullTypeName;
+
+	public String getProductCode() {
+		return productCode;
+	}
+
+	public void setProductCode(String productCode) {
+		this.productCode = productCode;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getProductTypeName() {
+		return productTypeName;
+	}
+
+	public void setProductTypeName(String productTypeName) {
+		this.productTypeName = productTypeName;
+	}
+
+	public boolean isNeedFullTypeName() {
+		return needFullTypeName;
+	}
+
+	public void setNeedFullTypeName(boolean needFullTypeName) {
+		this.needFullTypeName = needFullTypeName;
+	}
+	
+	
+
+}
