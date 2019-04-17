8a3a26aa8e31c35cecd314ddc314c3e3180a2233
tangjun <tangjun@CN-20170216CTDG>
2019-04-17 17:18:39 +0800

    payment

---
+++ orders/sql/5.create_payment_table.sql
@@ -0,0 +1,31 @@
+create table PAYMENT_TXN
+(
+  PAYMENT_ID BIGINT primary key not null auto_increment comment '支付流水主键',
+  payer_no VARCHAR(18) not null comment '付款方会员号(18个‘0’表示平台方)',
+  payee_no VARCHAR(18) not null comment '收款方会员号(18个‘0’表示平台方)',
+  pay_type char(3) not null comment '支付方式(淘宝，支付宝等)',
+  amt    DECIMAL(22) not null comment '支付额',
+  pay_order_no VARCHAR(18) comment '支付单号',
+  sub_order_no VARCHAR(20) comment '订单号',
+  incident VARCHAR(256) comment '事由',
+  remark VARCHAR(256) comment '备注',
+  deal_status char(1) comment '处理状态：“0:待处理;1:已处理分户账”',
+  insert_time TIMESTAMP not null DEFAULT NOW() comment '插入时间',
+  update_time TIMESTAMP not null DEFAULT NOW() comment '修改时间'
+) comment='支付流水表';
+
+create table HOUSEHOLD_ACC_TXN
+(
+  TXN_ID BIGINT primary key not null auto_increment comment '流水主键',
+  ACC_NO VARCHAR(18) not null comment '账户号（即各个商户号，18个‘0’表示平台方）',
+  ACC_FLG char(1) not null comment '借贷符号(+表示借，-表示贷)',
+  pay_order_no VARCHAR(18) comment '支付单号',
+  sub_order_no VARCHAR(20) comment '订单号',
+  IS_CURRENT char(1) not null comment '是否最后一笔流水',
+  amt    DECIMAL(22) comment '支付额',
+  cash_balance DECIMAL(22) comment '现金余额',
+  remark VARCHAR(256) not null comment '备注',
+  insert_time TIMESTAMP not null DEFAULT NOW() comment '插入时间',
+  update_time TIMESTAMP not null DEFAULT NOW() comment '修改时间'
+) comment='分户账支付流水表';
+
--- orders/src/main/java/com/xiaoke1256/orders/core/service/PaymentService.java
+++ orders/src/main/java/com/xiaoke1256/orders/core/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.xiaoke1256.orders.core.service;
 
+import java.sql.Timestamp;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -9,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.xiaoke1256.orders.common.exception.BusinessException;
 import com.xiaoke1256.orders.common.exception.ErrorCode;
 import com.xiaoke1256.orders.core.bo.PayOrder;
+import com.xiaoke1256.orders.core.bo.PaymentTxn;
 import com.xiaoke1256.orders.core.bo.SubOrder;
 
 /**
@@ -28,7 +31,7 @@ public class PaymentService {
 	 * @param payOrderNo 支付单号
 	 * @param verifyInfo 支付校验信息
 	 */
-	public void pay(String payOrderNo,String verifyInfo) {
+	public void pay(String payOrderNo,String payType,String verifyInfo) {
 		PayOrder payOrder = entityManager.find(PayOrder.class, payOrderNo);
 		if(!PayOrder.ORDER_STATUS_PAYING.equals(payOrder.getStatus())) {
 			throw new BusinessException(ErrorCode.BUSSNESS_ERROR.getCode(),"The order has payed","该订单已经支付过了。");
@@ -37,6 +40,17 @@ public class PaymentService {
 		//TODO 调用第三方支付接口校验支付情况
 		
 		//记录支付流水
+		PaymentTxn paymentTxn = new PaymentTxn();
+		paymentTxn.setPayType(payType);
+		paymentTxn.setPayeeNo("000000000000000000");
+		paymentTxn.setPayerNo(payOrder.getPayerNo());
+		paymentTxn.setAmt(payOrder.getTotalAmt());
+		paymentTxn.setPayOrderNo(payOrder.getPayOrderNo());
+		paymentTxn.setIncident("购买商品");
+		paymentTxn.setInsertTime(new Timestamp(System.currentTimeMillis()));
+		paymentTxn.setUpdateTime(new Timestamp(System.currentTimeMillis()));
+		entityManager.persist(paymentTxn);
+		
 		//修改订单状态
 		payOrder.setStatus(PayOrder.ORDER_STATUS_PAYED);
 		entityManager.merge(payOrder);
@@ -44,5 +58,6 @@ public class PaymentService {
 			subOrder.setStatus(SubOrder.ORDER_STATUS_AWAIT_ACCEPT);
 			entityManager.merge(subOrder);
 		}
+		//TODO 推送mq，通知其他系统。
 	}
 }
