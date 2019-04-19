5bdd7ca0d6b49193399fd90cd14e1ed210c13000
tangjun <tangjun@CN-20170216CTDG>
2019-04-19 15:49:52 +0800

    第三方支付表。

--- orders/sql/6.create_third_pay_order_table.sql
+++ orders/sql/6.create_third_pay_order_table.sql
@@ -4,6 +4,8 @@ create table THIRD_PAY_ORDER
   ORDER_NO VARCHAR(20) not null unique comment '订单号',
   payer_no VARCHAR(18) not null comment '付款方会员号(第三方平台的)',
   payee_no VARCHAR(18) not null comment '收款方会员号(第三方平台的)',
+  order_type VARCHAR(2) not null comment '订单类型：01-消费;02-退货款;03-与平台方结算;04-理财;05-结息',
+  order_status VARCHAR(2) not null comment '状态：00-受理支付;99-失败;90-成功',
   amt    DECIMAL(22) not null comment '支付额',
   incident VARCHAR(256) comment '事由',
   remark VARCHAR(256) comment '备注',
