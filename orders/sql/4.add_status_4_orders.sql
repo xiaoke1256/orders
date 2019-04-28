1cacda6030fb332dbc0ba301568d06d6607d8da0
tangjun <tangjun@CN-20170216CTDG>
2019-04-28 17:49:06 +0800

    comment

--- orders/sql/4.add_status_4_orders.sql
+++ orders/sql/4.add_status_4_orders.sql
@@ -1,3 +1,3 @@
 alter table PAY_ORDER add STATUS char(1) default '0' comment '0:未支付;1:已支付';
 
-alter table SUB_ORDER add STATUS char(1) default '0' comment '0:待支付;1:待备货;2:待发货;3:待清算;4:待打款;9:结束';
+alter table SUB_ORDER add STATUS char(1) default '0' comment '0:待支付;2:待发货;3:待清算;4:待打款;9:结束';
