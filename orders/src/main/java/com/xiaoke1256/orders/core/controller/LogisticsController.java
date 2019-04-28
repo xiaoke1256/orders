947fed524ea8ed60da175a16af5c34e1e74027c2
tangjun <tangjun@CN-20170216CTDG>
2019-04-28 17:52:11 +0800

    logistics.

---
+++ orders/src/main/java/com/xiaoke1256/orders/core/controller/LogisticsController.java
@@ -0,0 +1,15 @@
+package com.xiaoke1256.orders.core.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 物流模块
+ * @author Administrator
+ *
+ */
+@Controller
+@RequestMapping("/logistics")
+public class LogisticsController {
+	
+}
