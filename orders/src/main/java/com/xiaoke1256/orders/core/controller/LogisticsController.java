b98c9c2d8cdfaf00c4fb9c6eee15beaa2836426d
tangjun <tangjun@CN-20170216CTDG>
2019-05-03 15:40:59 +0800

    remove orders bo.

--- orders/src/main/java/com/xiaoke1256/orders/core/controller/LogisticsController.java
+++ orders/src/main/java/com/xiaoke1256/orders/core/controller/LogisticsController.java
@@ -1,6 +1,6 @@
 package com.xiaoke1256.orders.core.controller;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
