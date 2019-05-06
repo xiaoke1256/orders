d3cfd8ec18ec979e91379345f8f0f8209e4fc76d
tangjun <tangjun@CN-20170216CTDG>
2019-05-06 16:19:03 +0800

    store query.

---
+++ product-api/src/main/java/com/xiaoke1256/orders/product/api/StoreQueryService.java
@@ -0,0 +1,16 @@
+package com.xiaoke1256.orders.product.api;
+
+import java.util.List;
+
+import org.apache.catalina.Store;
+
+/**
+ * 与商铺有关的查询
+ * @author Administrator
+ *
+ */
+public interface StoreQueryService {
+	
+	/**获取有用的（未关闭的）商铺*/
+	public List<Store> queryAvailableStore();
+}
