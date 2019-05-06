4832eb3cec43563445377987560ef56da54c657e
tangjun <tangjun@CN-20170216CTDG>
2019-05-06 17:25:05 +0800

    settle.

--- orders-task/src/main/java/com/xiaoke1256/orders/core/task/SettlementTask.java
+++ orders-task/src/main/java/com/xiaoke1256/orders/core/task/SettlementTask.java
@@ -3,10 +3,13 @@ package com.xiaoke1256.orders.core.task;
 import java.util.Date;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.xiaoke1256.orders.common.util.DateUtil;
+import com.xiaoke1256.orders.core.service.SettleService;
 import com.xiaoke1256.orders.product.api.StoreQueryService;
 import com.xiaoke1256.orders.product.dto.Store;
 
@@ -17,14 +20,32 @@ import com.xiaoke1256.orders.product.dto.Store;
  */
 @Component
 public class SettlementTask {
+	private static final Logger logger = LoggerFactory.getLogger(SendingExpiredTask.class);
 	
 	@Autowired
 	private StoreQueryService storeQueryService;
 	
+	@Autowired
+	private SettlementTaskWatcher settlementTaskWatcher;
+	
+	@Autowired
+	private SettleService settleService;
+	
+	public void startSettlement() {
+		try {
+			if(!settlementTaskWatcher.toBeMast()) {
+				return;
+			}
+			settlement();
+		} catch (InterruptedException e) {
+			logger.error(e.getMessage(), e);
+		}
+	}
+	
 	/**
 	 * 清算
 	 */
-	public void settlement() {
+	private void settlement() {
 		//远程调用查询商户信息
 		List<Store> stores = storeQueryService.queryAvailableStore();
 		//根据当前日期，决定清算的报告期。规则：小于5号，则清算上一月度；大于5号则清算本月度
@@ -34,5 +55,8 @@ public class SettlementTask {
 			reportDate = DateUtil.addMonth(reportDate, -1);
 		String year = String.valueOf(DateUtil.getYear(reportDate));
 		String month = String.valueOf(DateUtil.getMonth(reportDate));
+		for(Store store:stores) {
+			settleService.genSettleStatemt(store.getStoreNo(), year, month);
+		}
 	}
 }
