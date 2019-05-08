2e206bc89fa462ba7ddcae61d2a4abb7ffd49d03
tangjun <tangjun@CN-20170216CTDG>
2019-05-08 18:18:24 +0800

    search for make money.

--- orders/src/main/java/com/xiaoke1256/orders/core/controller/OrderController.java
+++ orders/src/main/java/com/xiaoke1256/orders/core/controller/OrderController.java
@@ -99,6 +99,25 @@ public class OrderController {
 		voResult.setResultList(voList);
 		return new OrderQueryResultResp(voResult);
 	}
+	
+	/**
+	 * 为了打款而进行的订单查询
+	 * @param condition
+	 * @return
+	 */
+	@RequestMapping(value="/searchForMakeMoney",method={RequestMethod.GET})
+	@ResponseBody
+	public RespMsg searchOrdersForMakeMoney(OrderCondition condition) {
+		condition.setStatus(SubOrder.ORDER_STATUS_AWAIT_MAKE_MONEY);
+		QueryResult<SubOrder> queryResult = orederService.searchOrderByCondition(condition);
+		List<com.xiaoke1256.orders.core.dto.SubOrder> voList = new ArrayList<com.xiaoke1256.orders.core.dto.SubOrder>();
+		for(SubOrder subOrder:queryResult.getResultList()) {
+			voList.add(covertToVo(subOrder));
+		}
+		QueryResult<com.xiaoke1256.orders.core.dto.SubOrder> voResult = new QueryResult<com.xiaoke1256.orders.core.dto.SubOrder>(queryResult.getPageNo(),queryResult.getPageSize(),queryResult.getTotalCount());
+		voResult.setResultList(voList);
+		return new OrderQueryResultResp(voResult);
+	}
 
 	/**
 	 * 子订单转化成Vo
