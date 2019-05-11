f7fb288b209c5930d4521a356710508c736c0c72
tangjun <tangjun@CN-20170216CTDG>
2019-05-11 22:20:11 +0800

    3des encode and decode.

--- common/src/main/java/com/xiaoke1256/orders/common/security/ThreeDESUtil.java
+++ common/src/main/java/com/xiaoke1256/orders/common/security/ThreeDESUtil.java
@@ -1,14 +1,13 @@
 package com.xiaoke1256.orders.common.security;
 
+import java.util.Base64;
+
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.DESKeySpec;
 import javax.crypto.spec.DESedeKeySpec;
 import javax.crypto.spec.IvParameterSpec;
-
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
 
 public class ThreeDESUtil {
     private static final String IV = "1234567-";
@@ -41,8 +40,7 @@ public class ThreeDESUtil {
         final byte[] b = cipher.doFinal(src.getBytes("UTF-8"));
 
         // --通过base64,将加密数组转换成字符串
-        final BASE64Encoder encoder = new BASE64Encoder();
-        return encoder.encode(b);
+        return Base64.getEncoder().encodeToString(b);
     }
 
     /**
@@ -57,8 +55,7 @@ public class ThreeDESUtil {
      */
     public static String decryptDESCBC(final String src, final String key) throws Exception {
         // --通过base64,将字符串转成byte数组
-        final BASE64Decoder decoder = new BASE64Decoder();
-        final byte[] bytesrc = decoder.decodeBuffer(src);
+        final byte[] bytesrc = Base64.getDecoder().decode(src);
 
         // --解密的key
         final DESKeySpec desKeySpec = new DESKeySpec(key.getBytes("UTF-8"));
@@ -88,16 +85,14 @@ public class ThreeDESUtil {
         cipher.init(Cipher.ENCRYPT_MODE, securekey);
         final byte[] b = cipher.doFinal(src.getBytes());
 
-        final BASE64Encoder encoder = new BASE64Encoder();
-        return encoder.encode(b).replaceAll("\r", "").replaceAll("\n", "");
+        return Base64.getEncoder().encodeToString(b).replaceAll("\r", "").replaceAll("\n", "");
 
     }
 
     // 3DESECB解密,key必须是长度大于等于 3*8 = 24 位哈
     public static String decryptThreeDESECB(final String src, final String key) throws Exception {
         // --通过base64,将字符串转成byte数组
-        final BASE64Decoder decoder = new BASE64Decoder();
-        final byte[] bytesrc = decoder.decodeBuffer(src);
+        final byte[] bytesrc = Base64.getDecoder().decode(src);
         // --解密的key
         final DESedeKeySpec dks = new DESedeKeySpec(key.getBytes("UTF-8"));
         final SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DESede");
