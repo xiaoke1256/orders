941c888efd269326ba3f60bce2c676773c4cfd24
tangjun <tangjun@CN-20170216CTDG>
2019-05-17 13:48:02 +0800

    zookeeper crate meta node.

--- common-zookeeper/src/main/java/com/xiaoke1256/orders/common/zookeeper/MasterWatcher.java
+++ common-zookeeper/src/main/java/com/xiaoke1256/orders/common/zookeeper/MasterWatcher.java
@@ -2,10 +2,14 @@ package com.xiaoke1256.orders.common.zookeeper;
 
 import java.util.Random;
 
+import org.apache.zookeeper.AsyncCallback.StringCallback;
+import org.apache.zookeeper.KeeperException.Code;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooDefs.Ids;
 import org.apache.zookeeper.data.Stat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 选举为群主的zookeeper主键
@@ -13,6 +17,8 @@ import org.apache.zookeeper.data.Stat;
  *
  */
 public class MasterWatcher extends BaseWatcher {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MasterWatcher.class);
 	
 	private volatile Boolean isMaster;
 	
@@ -26,22 +32,37 @@ public class MasterWatcher extends BaseWatcher {
 		this.nodePath = nodePath;
 	}
 
-	public synchronized boolean toBeMast() throws InterruptedException {
+	public boolean toBeMast() throws InterruptedException {
+		return toBeMast(false);
+	}
+	
+	/**
+	 * 竞选成为主节点
+	 * @param needWorker 是否需要从节点
+	 * @return
+	 * @throws InterruptedException
+	 */
+	public synchronized boolean toBeMast(boolean needWorker) throws InterruptedException {
 		isMaster = null;
 		while(true) {
 			try {
 				this.zooKeeper.create(nodePath, serverId.getBytes(),Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
 				isMaster = true;
+				bootstrap(needWorker);
 				return isMaster;
 			} catch (KeeperException e) {
 				switch (e.code()) {
 				case NODEEXISTS:
-					if(checkMaster(nodePath))
+					if(checkMaster(nodePath)) {
+						bootstrap(isMaster&&needWorker);
 						return isMaster;
+					}
 				default:
 					//其他异常（含ConnectLossException）。
-					if(checkMaster(nodePath))
+					if(checkMaster(nodePath)) {
+						bootstrap(isMaster&&needWorker);
 						return isMaster;
+					}
 				}
 			}
 		}
@@ -68,8 +89,45 @@ public class MasterWatcher extends BaseWatcher {
 					//其他异常（含ConnectLossException）. Read it again.TODO 需打日志。
 				}
 			}
-		}
-			
-		
+		}	
 	}
+	
+	/**
+	 * crate meta node
+	 */
+	private void bootstrap(boolean needWorker) {
+		if(!needWorker)
+			return;
+		String basePath = nodePath;
+		if(basePath.lastIndexOf("/master")>=0)
+			basePath.substring(0, basePath.lastIndexOf("/master"));
+		createParent(basePath+"/workers",new byte[0]);
+		createParent(basePath+"/assign",new byte[0]);
+		createParent(basePath+"/tasks",new byte[0]);
+		createParent(basePath+"/status",new byte[0]);
+	}
+	
+	private void createParent(String path, byte[] data) {
+		zooKeeper.create(path, data, Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT, createParent, data);
+	}
+	
+	private StringCallback createParent = new StringCallback() {
+		@Override
+		public void processResult(int rc, String path, Object ctx, String name) {
+			switch(Code.get(rc)) {
+			case CONNECTIONLOSS:
+				createParent(path,(byte[])ctx);
+				break;
+			case OK:
+				logger.info("Parent created");
+				break;
+			case NODEEXISTS:
+				logger.warn("Parent already rigistered: %s",path);
+				break;
+			default:
+				logger.error("Something went wrong: ",KeeperException.create(Code.get(rc),path));
+			}
+		}
+		
+	};
 }
