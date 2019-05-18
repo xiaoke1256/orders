a356ecc5fde1f8b2784c98a2c81baf518ac0d901
tangjun <tangjun@CN-20170216CTDG>
2019-05-18 23:26:56 +0800

    assign task.

--- common-zookeeper/src/main/java/com/xiaoke1256/orders/common/zookeeper/Worker.java
+++ common-zookeeper/src/main/java/com/xiaoke1256/orders/common/zookeeper/Worker.java
@@ -1,9 +1,15 @@
 package com.xiaoke1256.orders.common.zookeeper;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 import org.apache.zookeeper.AsyncCallback.ChildrenCallback;
+import org.apache.zookeeper.AsyncCallback.DataCallback;
 import org.apache.zookeeper.AsyncCallback.StatCallback;
 import org.apache.zookeeper.AsyncCallback.StringCallback;
 import org.apache.zookeeper.KeeperException.Code;
@@ -26,6 +32,8 @@ public class Worker extends BaseWatcher {
 	private String baseNodePath;
 	
 	private String status = "Idle";
+	
+	private Executor executor = Executors.newFixedThreadPool(10);
 
 	
 	/**
@@ -85,5 +93,80 @@ public class Worker extends BaseWatcher {
 		
 	}
 	
+	private Watcher newTaskWatcher = new Watcher() {
+
+		@Override
+		public void process(WatchedEvent event) {
+			if(EventType.NodeChildrenChanged.equals(event.getType())) {
+				assert new String(baseNodePath+"/assign/worker-"+serverId).equals(event.getPath());
+				getTasks();
+			}
+		}
+	};
+	
+	private void getTasks() {
+		zooKeeper.getChildren(baseNodePath+"/assign/worker-"+serverId, newTaskWatcher,tasksGetChildrenCallback,null);
+	}
+	
+	private Set<String> onGoingTasks = new HashSet<>();
+	
+	private ChildrenCallback tasksGetChildrenCallback = new ChildrenCallback() {
+
+		@Override
+		public void processResult(int rc, String path, Object ctx, List<String> children) {
+			switch(Code.get(rc)) {
+			case CONNECTIONLOSS:
+				getTasks();
+				break;
+			case OK:
+				if(children!=null) {
+					executor.execute(() -> {
+						synchronized (onGoingTasks) {
+							for(String task:children) {
+								if(!onGoingTasks.contains(task)) {
+									logger.trace("New task {}",task);
+									getTaskData(task);
+									onGoingTasks.add(task);
+								}
+							}
+						}
+						
+					});
+				}
+				break;
+			default:
+				logger.error("Get children fail: ",KeeperException.create(Code.get(rc),path));
+			}
+		}
+		
+	};
+	
+	private void getTaskData(String task) {
+		zooKeeper.getData(baseNodePath+"/assign/worker-"+serverId+"/"+task, false, excuteTaskCallback,task);
+	}
+	
+	private DataCallback excuteTaskCallback = new DataCallback() {
+
+		@Override
+		public void processResult(int rc, String path, Object ctx, byte[] data, Stat stat) {
+			// TODO Auto-generated method stub
+			switch(Code.get(rc)){
+			case CONNECTIONLOSS:
+				getTaskData((String)ctx);
+				break;
+			case OK:
+				doBusiness((String)ctx);
+				break;
+			default:
+			}
+		}
+		
+	};
+
+
+	protected void doBusiness(String ctx) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
