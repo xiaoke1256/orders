4a3cc8dcd4326ca277246c91c0a6c7bf01796d07
tangjun <tangjun@CN-20170216CTDG>
2019-06-15 17:15:29 +0800

    remove useless import.

--- orders-task/src/main/java/com/xiaoke1256/orders/core/task/HouseholdTask.java
+++ orders-task/src/main/java/com/xiaoke1256/orders/core/task/HouseholdTask.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.xiaoke1256.orders.common.zookeeper.MasterWatcher;
-import com.xiaoke1256.orders.core.bo.PaymentTxn;
 import com.xiaoke1256.orders.core.service.AccService;
 
 /**
