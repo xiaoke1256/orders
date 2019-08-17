2d3353fe90dc014d39cb63a9e5b4619e8100af72
tangjun <tangjun@CN-20170216CTDG>
2019-08-17 11:23:54 +0800

    submit 3rdpay.war to discovery.

--- 3rdPay/src/main/java/com/xiaoke1256/thirdpay/SpringbootApplication.java
+++ 3rdPay/src/main/java/com/xiaoke1256/thirdpay/SpringbootApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 /**
  * 本应用用来模拟第三方支付平台
  * @author Administrator
@@ -14,6 +15,7 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
  */
 @SpringBootApplication(exclude={HibernateJpaAutoConfiguration.class})
 @EnableAutoConfiguration
+@EnableDiscoveryClient
 @MapperScan("com.xiaoke1256.thirdpay.payplatform.dao")
 public class SpringbootApplication extends SpringBootServletInitializer {
 	
