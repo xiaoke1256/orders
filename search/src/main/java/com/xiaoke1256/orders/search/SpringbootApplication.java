304419c51d5929ce4f47d0a81cedcf93b63704d0
tangjun <tangjun@CN-20170216CTDG>
2019-08-17 22:51:35 +0800

    remove useless import.

--- search/src/main/java/com/xiaoke1256/orders/search/SpringbootApplication.java
+++ search/src/main/java/com/xiaoke1256/orders/search/SpringbootApplication.java
@@ -1,7 +1,6 @@
 package com.xiaoke1256.orders.search;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,8 +8,6 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
 
 @EnableDiscoveryClient
 @SpringBootApplication(exclude={HibernateJpaAutoConfiguration.class})
