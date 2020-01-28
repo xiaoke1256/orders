528cf3a515e9432d2a6b720b33fb1fa6ccafe42d
tangjun <tangjun@CN-20170216CTDG>
2020-01-28 21:29:44 +0800

    set SessionStorage.

--- orders-store-web/orders-store-web/src/views/login.vue
+++ orders-store-web/orders-store-web/src/views/login.vue
@@ -24,7 +24,7 @@ export default {
         return false;
       }
       //把token放到sessionStorage.
-      
+      window.sessionStorage.setItem("storeNo", this.storeNo);
       this.$router.push('/home');
     }
   },
