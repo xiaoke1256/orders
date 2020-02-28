d77eac6c5a7b0ceef297ed91daf12008a610abe4
tangjun <tangjun@CN-20170216CTDG>
2020-02-28 23:31:07 +0800

    rest controller

--- product/src/main/java/com/xiaoke1256/orders/member/controller/MemberController.java
+++ product/src/main/java/com/xiaoke1256/orders/member/controller/MemberController.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.xiaoke1256.orders.member.dto.Member;
 import com.xiaoke1256.orders.member.service.MemberService;
 
-@Controller
+@RestController
 public class MemberController {
 	
 	@Autowired
