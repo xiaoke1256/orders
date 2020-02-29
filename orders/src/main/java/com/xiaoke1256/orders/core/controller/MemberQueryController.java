1fa26f752021d5bf09f792c90ffa5f6b71b5a818
tangjun <tangjun@CN-20170216CTDG>
2020-02-29 19:10:10 +0800

    members query controller

--- orders/src/main/java/com/xiaoke1256/orders/core/controller/MemberQueryController.java
+++ orders/src/main/java/com/xiaoke1256/orders/core/controller/MemberQueryController.java
@@ -2,18 +2,23 @@ package com.xiaoke1256.orders.core.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.xiaoke1256.orders.core.client.MemberQueryClient;
 import com.xiaoke1256.orders.member.api.MemberQueryService;
 import com.xiaoke1256.orders.member.dto.Member;
 
 @RestController
 public class MemberQueryController implements MemberQueryService {
 
+	private MemberQueryClient memberQueryClient;
+	
 	@Override
+	@RequestMapping(value="/members",method={RequestMethod.GET})
 	public List<Member> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return memberQueryClient.findAll();
 	}
 
 }
