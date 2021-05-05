02b029ee4daf545840d47d035baf403d53aaa449
xiaoke_1256 <xiaoke_1256@sina.com>
2021-05-05 22:02:39 +0800

    登录界面

--- orders-web-shop/orders-web-shop/src/router/index.ts
+++ orders-web-shop/orders-web-shop/src/router/index.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld.vue'
+import Login from '@/view/Login.vue'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Login',
+      component: Login
     }
   ]
 })
---
+++ orders-web-shop/orders-web-shop/src/view/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      {{msg}}
+  </div>
+</template>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class Login extends Vue {
+  public msg='这是登录页面';
+
+}
+</script>
