bf7c09820589147ec64a0385e07f0114c4057f4a
xiaoke_1256 <xiaoke_1256@sina.com>
2021-05-06 21:58:43 +0800

    导入iview

--- orders-web-shop/orders-web-shop/src/view/Login.vue
+++ orders-web-shop/orders-web-shop/src/view/Login.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
       {{msg}}
+      <Row>
+        <Col span="12">
+          <Input v-model="userName"/>
+        </Col>
+      </Row>
   </div>
 </template>
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
+import { Input,Row,Col } from 'iview'; 
 
-@Component
+@Component({components:{Input,Row,Col}})
 export default class Login extends Vue {
   public msg='这是登录页面';
-
+  public userName:string="";
 }
 </script>
