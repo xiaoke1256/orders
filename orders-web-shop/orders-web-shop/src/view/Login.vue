6f13e877f7bfe73e187ad3bcbcd00dac6b137f55
xiaoke_1256 <xiaoke_1256@sina.com>
2021-05-16 22:07:30 +0800

    标题

--- orders-web-shop/orders-web-shop/src/view/Login.vue
+++ orders-web-shop/orders-web-shop/src/view/Login.vue
@@ -6,7 +6,7 @@
     border-radius: 4px;
 }
 .layout-logo{
-    width: 100px;
+    width: 150px;
     height: 30px;
     background: #5b6270;
     border-radius: 3px;
@@ -14,6 +14,9 @@
     position: relative;
     top: 15px;
     left: 20px;
+    color:lightsteelblue;
+    font:italic bold 40px  "sans-serif" ;
+
 }
 .layout-nav{
     width: 420px;
@@ -29,25 +32,24 @@
     <Layout :style="{minHeight: '99.6vh'}" >
         <Header>
           <Menu mode="horizontal" theme="dark" active-name="1">
-            <div class="layout-logo"></div>
+            <div class="layout-logo">Orders</div>
+            <!--
             <div class="layout-nav">
-                <MenuItem name="1">
-                    <Icon type="ios-navigate"></Icon>
-                    Item 1
-                </MenuItem>
-                <MenuItem name="2">
-                    <Icon type="ios-keypad"></Icon>
-                    Item 2
-                </MenuItem>
-                <MenuItem name="3">
-                    <Icon type="ios-analytics"></Icon>
-                    Item 3
-                </MenuItem>
-                <MenuItem name="4">
-                    <Icon type="ios-paper"></Icon>
-                    Item 4
-                </MenuItem>
+              <MenuItem name="1">
+                <Icon type="ios-navigate"></Icon>
+                公司首页
+              </MenuItem>
+              <MenuItem name="2">
+                <Icon type="ios-keypad"></Icon>
+                网站地图
+              </MenuItem>
+              <MenuItem name="3">
+                <Icon type="ios-analytics"></Icon>
+                意见与建议
+              </MenuItem>
             </div>
+            -->
+            <!--欢迎访问商户端-->
           </Menu>
         </Header>
         <Content :style="{padding: '20px 50px'}">
@@ -57,7 +59,7 @@
             </div>
           </Card>
         </Content>
-        <Footer class="layout-footer-center">2011-2016 &copy; TalkingData</Footer>
+        <Footer class="layout-footer-center">2021 &copy; xiaoke1256</Footer>
     </Layout>
   </div>
 </template>
