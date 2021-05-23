7ad40d690ee3f0906aa4682b134420c5bcca5a5b
xiaoke_1256 <xiaoke_1256@sina.com>
2021-05-23 22:49:14 +0800

    maven

---
+++ orders_store_intra/src/main/java/com/xiaoke1256/orders/store/intra/SpringbootApplication.java
@@ -0,0 +1,26 @@
+package com.xiaoke1256.orders.store.intra;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication(exclude={HibernateJpaAutoConfiguration.class})
+@EnableAutoConfiguration
+@MapperScan("com.xiaoke1256.orders.*.dao")
+public class SpringbootApplication extends SpringBootServletInitializer {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringbootApplication.class);
+    }
+	
+	public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+}
