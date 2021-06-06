10da0035c559783f972441b7d301e505f0592722
xiaoke_1256 <xiaoke_1256@sina.com>
2021-06-06 11:21:05 +0800

    controller

--- orders_store_intra/src/main/java/com/xiaoke1256/orders/store/intra/cotroller/LoginController.java
+++ orders_store_intra/src/main/java/com/xiaoke1256/orders/store/intra/cotroller/LoginController.java
@@ -1,4 +1,10 @@
 package com.xiaoke1256.orders.store.intra.cotroller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class LoginController {
+    public String login(){
+        return null;
+    }
 }
