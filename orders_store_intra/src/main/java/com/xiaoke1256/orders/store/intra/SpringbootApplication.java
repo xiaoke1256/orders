a630bf7b2044e513c64eeab0c53c5dc2147da89a
xiaoke_1256 <xiaoke_1256@sina.com>
2021-06-06 22:47:34 +0800

    spring boot

--- orders_store_intra/src/main/java/com/xiaoke1256/orders/store/intra/SpringbootApplication.java
+++ orders_store_intra/src/main/java/com/xiaoke1256/orders/store/intra/SpringbootApplication.java
@@ -4,15 +4,15 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@SpringBootApplication(exclude={HibernateJpaAutoConfiguration.class})
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 @EnableAutoConfiguration
-@MapperScan("com.xiaoke1256.orders.*.dao")
 public class SpringbootApplication extends SpringBootServletInitializer {
 	
 	@Override
@@ -21,6 +21,6 @@ public class SpringbootApplication extends SpringBootServletInitializer {
     }
 	
 	public static void main(String[] args) {
-        SpringApplication.run(SpringbootApplication.class, args);
+	    SpringApplication.run(SpringbootApplication.class, args);
     }
 }
