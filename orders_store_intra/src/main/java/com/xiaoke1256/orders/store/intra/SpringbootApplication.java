a5bb9b912a4d309d71b9ccaf7f3396476548b0a2
xiaoke_1256 <xiaoke_1256@sina.com>
2021-06-13 22:38:00 +0800

    spring boot config

--- orders_store_intra/src/main/java/com/xiaoke1256/orders/store/intra/SpringbootApplication.java
+++ orders_store_intra/src/main/java/com/xiaoke1256/orders/store/intra/SpringbootApplication.java
@@ -1,10 +1,11 @@
 package com.xiaoke1256.orders.store.intra;
 
-import org.mybatis.spring.annotation.MapperScan;
+//import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
@@ -12,7 +13,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class SpringbootApplication extends SpringBootServletInitializer {
 	
 	@Override
