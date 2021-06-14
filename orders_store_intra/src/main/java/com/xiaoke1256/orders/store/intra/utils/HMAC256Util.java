caf24b4d9fed6035b55534925764f20207ad72cc
xiaoke_1256 <xiaoke_1256@sina.com>
2021-06-14 18:00:54 +0800

    token 算法

---
+++ orders_store_intra/src/main/java/com/xiaoke1256/orders/store/intra/utils/HMAC256Util.java
@@ -0,0 +1,41 @@
+package com.xiaoke1256.orders.store.intra.utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HMAC256Util {
+
+    //设置过期时间
+    private static final long EXPIRE_DATE=30*60*100000;
+    //token秘钥
+    private static final String TOKEN_SECRET = "ZCEQIUBFKSJBFJH2020BQWE";
+
+    public static String token (String username,String password){
+
+        String token = "";
+        try {
+            //过期时间
+            Date date = new Date(System.currentTimeMillis()+EXPIRE_DATE);
+            //秘钥及加密算法
+            Algorithm algorithm = Algorithm.HMAC256(TOKEN_SECRET);
+            //设置头部信息
+            Map<String,Object> header = new HashMap<>();
+            header.put("typ","JWT");
+            header.put("alg","HS256");
+            //携带username，password信息，生成签名
+            token = JWT.create()
+                    .withHeader(header)
+                    .withClaim("username",username)
+                    .withClaim("password",password).withExpiresAt(date)
+                    .sign(algorithm);
+        }catch (Exception e){
+            e.printStackTrace();
+            return  null;
+        }
+        return token;
+    }
+}
