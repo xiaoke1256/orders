84aa6468a31b38cc07bb8989f428f4bbbf257a99
xiaoke_1256 <xiaoke_1256@sina.com>
2021-06-14 22:49:12 +0800

    HMAC256 算法

--- orders_store_intra/src/main/java/com/xiaoke1256/orders/store/intra/utils/HMAC256Util.java
+++ orders_store_intra/src/main/java/com/xiaoke1256/orders/store/intra/utils/HMAC256Util.java
@@ -1,7 +1,11 @@
 package com.xiaoke1256.orders.store.intra.utils;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 import java.util.Date;
 import java.util.HashMap;
@@ -12,7 +16,7 @@ public class HMAC256Util {
     //设置过期时间
     private static final long EXPIRE_DATE=30*60*100000;
     //token秘钥
-    private static final String TOKEN_SECRET = "ZCEQIUBFKSJBFJH2020BQWE";
+    private static final String TOKEN_SECRET = "XIAOKE1256JBFJH2021BQWE";
 
     public static String token (String username,String password){
 
@@ -38,4 +42,61 @@ public class HMAC256Util {
         }
         return token;
     }
+
+    /**
+     * 检验token是否正确
+     * @param **token**
+     * @return
+     */
+    public static boolean verify(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(TOKEN_SECRET);
+            JWTVerifier verifier = JWT.require(algorithm).build();
+            @SuppressWarnings("unused")
+            DecodedJWT jwt = verifier.verify(token);
+            return true;
+        } catch (Exception e){
+            return false;
+        }
+    }
+
+
+    /**
+     * 从token中获取username信息
+     * @param **token**
+     * @return
+     */
+    public static String getUserName(String token){
+        try {
+            DecodedJWT jwt = JWT.decode(token);
+            return jwt.getClaim("loginName").asString();
+        } catch (JWTDecodeException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * token是否过期
+     * @param token
+     */
+    public static Boolean checkExpire(String token) {
+        try {
+            DecodedJWT jwt = JWT.decode(token);
+            //获取token过期时间
+            Date expiretime = jwt.getExpiresAt();
+            String etStr = String.valueOf(expiretime.getTime());
+            //获取系统当前时间
+            String nowTime = String.valueOf(System.currentTimeMillis());
+            //如果系统当前时间超过token过期时间返回false
+            if(nowTime.compareTo(etStr)>0){
+                return false;
+            }
+            return true;
+        } catch (JWTDecodeException e){
+            e.printStackTrace();
+        }
+        return true;
+    }
+
 }
