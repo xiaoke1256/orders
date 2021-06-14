b1f9b6b95d3622164bccaee8a13e7a81d19fb179
xiaoke_1256 <xiaoke_1256@sina.com>
2021-06-14 18:01:43 +0800

    登录

--- orders_store_intra/src/main/java/com/xiaoke1256/orders/store/intra/cotroller/LoginController.java
+++ orders_store_intra/src/main/java/com/xiaoke1256/orders/store/intra/cotroller/LoginController.java
@@ -1,10 +1,14 @@
 package com.xiaoke1256.orders.store.intra.cotroller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 public class LoginController {
-    public String login(){
-        return null;
+    @PostMapping("login")
+    public String login(String loginName,String password){
+        return UUID.randomUUID().toString();
     }
 }
