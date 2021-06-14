d922fdaabab2b6b3dc8c3d0ec89674d5ce5a21cf
xiaoke_1256 <xiaoke_1256@sina.com>
2021-06-14 22:51:24 +0800

    HMAC256

--- orders_store_intra/src/main/java/com/xiaoke1256/orders/store/intra/utils/HMAC256Util.java
+++ orders_store_intra/src/main/java/com/xiaoke1256/orders/store/intra/utils/HMAC256Util.java
@@ -4,7 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTDecodeException;
-import com.auth0.jwt.interfaces.Claim;
+//import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 
 import java.util.Date;
