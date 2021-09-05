ea75c9faada76c0b24ef6d26c8c71dfcaafac698
xiaoke_1256 <xiaoke_1256@sina.com>
2021-09-05 22:54:06 +0800

    sessionStorage

--- orders-web-shop/orders-web-shop/src/axios/index.ts
+++ orders-web-shop/orders-web-shop/src/axios/index.ts
@@ -75,7 +75,8 @@ axiosInst.interceptors.request.use(
       // 但是即使token存在，也有可能token是过期的，所以在每次的请求头中携带token        
       // 后台根据携带的token判断用户的登录情况，并返回给我们对应的状态码        
       // 而后我们可以在响应拦截器中，根据状态码进行一些统一的操作。        
-    const token = localStorage.getItem('token');
+    const token = sessionStorage.getItem('token');
+    console.log("token in:"+token);
     token && (config.headers.Authorization = token)
     return config;
   },    
