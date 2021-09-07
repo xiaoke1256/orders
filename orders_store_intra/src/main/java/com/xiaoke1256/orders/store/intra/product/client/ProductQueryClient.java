a3fb3fad90eca5117d8d450dbc065f25581ff494
xiaoke_1256 <xiaoke_1256@sina.com>
2021-09-07 22:25:14 +0800

    支持本地开发

--- orders_store_intra/src/main/java/com/xiaoke1256/orders/store/intra/product/client/ProductQueryClient.java
+++ orders_store_intra/src/main/java/com/xiaoke1256/orders/store/intra/product/client/ProductQueryClient.java
@@ -4,16 +4,15 @@ import com.xiaoke1256.orders.product.api.ProductQueryService;
 import com.xiaoke1256.orders.product.dto.ProductCondition;
 import com.xiaoke1256.orders.product.dto.SimpleProduct;
 import com.xiaoke1256.orders.product.dto.SimpleProductQueryResultResp;
-import com.xiaoke1256.orders.store.intra.login.client.MemberQueryFallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="api-product",url="${remote.api.product.uri}",fallbackFactory= MemberQueryFallbackFactory.class,path="product")
+@FeignClient(name="api-product",url="${remote.api.product.uri}",fallbackFactory= ProductQueryFallbackFactory.class,path="product")
 public interface ProductQueryClient extends ProductQueryService {
     @GetMapping("/product/search")
-    SimpleProductQueryResultResp searchProductByCondition(ProductCondition condition);
+    public SimpleProductQueryResultResp searchProductByCondition(ProductCondition condition);
 
     @GetMapping("/simpleProduct/{productCode}")
-    SimpleProduct getSimpleProductByCode(@PathVariable String productCode);
+    public SimpleProduct getSimpleProductByCode(@PathVariable String productCode);
 }
