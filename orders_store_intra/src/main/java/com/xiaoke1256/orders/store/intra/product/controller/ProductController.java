9a8f66051e29700dae7cf21ab2ca1fc857580710
xiaoke_1256 <xiaoke_1256@sina.com>
2021-09-15 22:33:15 +0800

    异常

--- orders_store_intra/src/main/java/com/xiaoke1256/orders/store/intra/product/controller/ProductController.java
+++ orders_store_intra/src/main/java/com/xiaoke1256/orders/store/intra/product/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.xiaoke1256.orders.store.intra.product.controller;
 
+import com.xiaoke1256.orders.common.RespCode;
+import com.xiaoke1256.orders.common.exception.BusinessException;
 import com.xiaoke1256.orders.product.dto.ProductCondition;
 import com.xiaoke1256.orders.product.dto.SimpleProductQueryResultResp;
 import com.xiaoke1256.orders.store.intra.product.client.ProductQueryClient;
@@ -17,6 +19,13 @@ public class ProductController {
 
     @RequestMapping(method= RequestMethod.GET)
     public SimpleProductQueryResultResp searchProductByCondition(ProductCondition condition){
-        return productQueryService.searchProductByCondition(condition);
+        SimpleProductQueryResultResp result = productQueryService.searchProductByCondition(condition);
+        if(RespCode.SUCCESS.getCode().equals(result.getCode())){
+            return result;
+        }else if(RespCode.BUSSNESS_ERROR.getCode().equals(result.getCode())){
+            throw new BusinessException(result.getCode(),result.getMsg());
+        }else{
+            throw new RuntimeException(result.getCode()+":"+result.getMsg());
+        }
     }
 }
