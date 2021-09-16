3d6cdda272e01752e5cfb8272f146de605133885
xiaoke_1256 <xiaoke_1256@sina.com>
2021-09-16 23:29:15 +0800

    分页组件

--- orders-web-shop/orders-web-shop/src/api/product.ts
+++ orders-web-shop/orders-web-shop/src/api/product.ts
@@ -1,6 +1,6 @@
 import axiosInst from '@/axios';
 import {Product} from '@/types/product';
 
-export const getPorductList:() => Promise<{resultList:Product[]}> = ()=>{
+export const getPorductList:() => Promise<{totalCount:number,resultList:Product[]}> = ()=>{
     return axiosInst.get('/product').then((resp)=>resp.data);
 }
--- orders-web-shop/orders-web-shop/src/view/product/Table.vue
+++ orders-web-shop/orders-web-shop/src/view/product/Table.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="content" >
     <Table :columns="productCols" :data="porductList"></Table>
+    <Page :total="totalCount" :page-size="pageSize" @on-change="onPageChange" show-sizer show-total transfer />
   </div>
 </template>
 <script lang="ts" >
@@ -11,9 +12,28 @@ import {getPorductList} from '@/api/product';
 @Component({components:{}})
 export default class ProductTable extends Vue {
     public porductList = [] as Product[];
+    public pageNo = 1;
+    public pageSize = 10;
+    public totalCount = 0;
+
     public async mounted(){
-      let {resultList} = await getPorductList();
+      this.search();
+    }
+
+    public async search(){
+      let {totalCount,resultList} = await getPorductList();
       this.porductList = resultList;
+      this.totalCount = totalCount;
+    }
+
+    public onPageChange(pageNo:number){
+      this.pageNo = pageNo;
+      this.search();
+    }
+
+    public onPageSizeChange(pageSize:number){
+      this.pageSize = pageSize;
+      this.search();
     }
 
     public get productCols(){
@@ -30,3 +50,8 @@ export default class ProductTable extends Vue {
     }
 }
 </script>
+<style>
+.ivu-page{
+  text-align: left;
+}
+</style>
