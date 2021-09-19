a6cdfc0f52d319b565b08ea1e1026825413b749f
xiaoke_1256 <xiaoke_1256@sina.com>
2021-09-19 23:29:52 +0800

    建表

--- product/sql/20210919_store_member_map.sql
+++ product/sql/20210919_store_member_map.sql
@@ -1,1 +1,9 @@
 -- 店铺与成员的对应关系
+create table store_member_map (
+    store_no VARCHAR(32),
+    member_id BIGINT,
+    role CHAR(2),
+    is_default CHAR(1),
+    create_time TIMESTAMP not null  DEFAULT NOW(),
+    update_time TIMESTAMP not null  DEFAULT NOW()
+) comment = '店铺与成员的对应关系';
