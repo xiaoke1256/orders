54918717672c2ab150c0e2cd698316ffe26086b4
xiaoke_1256 <xiaoke_1256@sina.com>
2021-09-20 09:59:57 +0800

    建表语句

--- product/sql/20210919_store_member_map.sql
+++ product/sql/20210919_store_member_map.sql
@@ -1,9 +1,10 @@
--- 店铺与成员的对应关系
+-- 店铺与会员的对应关系
 create table store_member_map (
-    store_no VARCHAR(32),
-    member_id BIGINT,
-    role CHAR(2),
-    is_default CHAR(1),
-    create_time TIMESTAMP not null  DEFAULT NOW(),
-    update_time TIMESTAMP not null  DEFAULT NOW()
+    store_no VARCHAR(32) comment '商铺编号',
+    member_id BIGINT comment '会员编号',
+    role CHAR(2) comment '角色（1：店长；2：店员）',
+    is_default_store CHAR(1) comment '是否默认商店',
+    create_time TIMESTAMP not null  DEFAULT NOW() comment '创建时间',
+    update_time TIMESTAMP not null  DEFAULT NOW() comment '修改时间',
+    primary key(store_no,member_id)
 ) comment = '店铺与成员的对应关系';
