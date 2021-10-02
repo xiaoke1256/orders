37b701a6abee2b1bf4beb688df94072a873c2807
xiaoke_1256 <xiaoke_1256@sina.com>
2021-10-02 15:13:11 +0800

    从 Session 中获取用户

---
+++ orders_store_intra/src/main/java/com/xiaoke1256/orders/store/intra/common/utils/ApplicationContextUtil.java
@@ -0,0 +1,25 @@
+package com.xiaoke1256.orders.common.appcontext;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationContextUtil implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        ApplicationContextUtil.applicationContext = applicationContext;
+    }
+
+    public static <T> T getBean(Class<T> clazz) {
+        return applicationContext.getBean(clazz);
+    }
+
+    public static Object getBean(String beanName) {
+        return applicationContext.getBean(beanName);
+    }
+}
---
+++ orders_store_intra/src/main/java/com/xiaoke1256/orders/store/intra/common/utils/RequestUtil.java
@@ -0,0 +1,2 @@
+package com.xiaoke1256.orders.store.intra.common.utils;public class RequestUtil {
+}
