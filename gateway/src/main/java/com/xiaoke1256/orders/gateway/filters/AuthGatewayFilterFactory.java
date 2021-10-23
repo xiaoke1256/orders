9543dec653e82dcce354c429a0dc96316a728624
xiaoke_1256 <xiaoke_1256@sina.com>
2021-10-23 19:09:55 +0800

    在 getway增加校验

---
+++ gateway/src/main/java/com/xiaoke1256/orders/gateway/filters/AuthGatewayFilterFactory.java
@@ -0,0 +1,61 @@
+package com.xiaoke1256.orders.gateway.filters;
+
+import com.xiaoke1256.orders.common.RespCode;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import javax.servlet.http.HttpServletResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class AuthGatewayFilterFactory extends AbstractGatewayFilterFactory<AuthGatewayFilterFactory.Config> {
+
+    private static  final Logger LOG = LoggerFactory.getLogger(AuthGatewayFilterFactory.class);
+
+    @Override
+    public List<String> shortcutFieldOrder() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+            LOG.info("RequestURI:"+request.getURI());
+            if(request.getURI().getPath().startsWith("/login")){
+                return chain.filter(exchange);
+            }
+            String token = request.getHeaders().getFirst("Authorization");
+            if(StringUtils.isEmpty(token)){
+                response.getHeaders().add("Content-Type","application/json;charset=UTF-8");
+                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                DataBuffer bodyDataBuffer = response.bufferFactory().wrap(("{code:'" + RespCode.LOGIN_ERROR.getCode() + "',msg:'尚未登录'}").getBytes(StandardCharsets.UTF_8));
+                return response.writeWith(Mono.just(bodyDataBuffer));
+            }
+
+//            if(!loginTokenGenerator.verify(token)){
+//                response.getHeaders().add("Content-Type","application/json;charset=UTF-8");
+//                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+//                DataBuffer bodyDataBuffer = response.bufferFactory().wrap(("{code:'"+ RespCode.LOGIN_ERROR.getCode()+"',msg:'token失效'}").getBytes(StandardCharsets.UTF_8));
+//                return response.writeWith(Mono.just(bodyDataBuffer));
+//            }
+
+            return chain.filter(exchange);
+        });
+    }
+
+
+    public static class Config{}
+}
