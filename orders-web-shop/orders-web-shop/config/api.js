ccc36ad502ed544b7b9a2cae308d4d4dcf47b3e3
xiaoke_1256 <xiaoke_1256@sina.com>
2021-11-07 15:06:02 +0800

    上下文

--- orders-web-shop/orders-web-shop/config/api.js
+++ orders-web-shop/orders-web-shop/config/api.js
@@ -2,10 +2,10 @@ let apiUrl = '';
 console.log("process.env.NODE_PACK:"+process.env.NODE_PACK);
 switch(process.env.NODE_PACK) {
   case 'production':
-    apiUrl = '"http://peer1:8763"';
+    apiUrl = '"http://peer1:8763/store_intra"';
     break;
   default:
-    apiUrl = '"http://localhost:8763"';
+    apiUrl = '"http://localhost:8763/store_intra"';
     break;
 }
 module.exports = apiUrl
