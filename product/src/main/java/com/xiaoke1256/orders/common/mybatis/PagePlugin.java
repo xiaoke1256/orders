8d5526611e0dec58ce810916d0571411b45c75b3
xiaoke_1256 <xiaoke_1256@sina.com>
2021-11-17 22:47:48 +0800

    改写SQL。

--- product/src/main/java/com/xiaoke1256/orders/common/mybatis/PagePlugin.java
+++ product/src/main/java/com/xiaoke1256/orders/common/mybatis/PagePlugin.java
@@ -1,8 +1,11 @@
 package com.xiaoke1256.orders.common.mybatis;
 
+import com.xiaoke1256.orders.common.page.BaseCondition;
 import org.apache.ibatis.executor.statement.StatementHandler;
 import org.apache.ibatis.mapping.ParameterMapping;
 import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.SystemMetaObject;
 
 import java.sql.Connection;
 import java.util.Map;
@@ -21,8 +24,25 @@ public class PagePlugin implements Interceptor {
         if(po.getClass().isAssignableFrom(Map.class)){
             Map<String,Object> parameterObject = (Map<String,Object>)po;
             //每个value都拿出来看看是不是Page
+            for(Object obj:parameterObject.values()){
+                if(obj.getClass().isAssignableFrom(BaseCondition.class)){
+                    //处理sql
+                    MetaObject metaStamtHandler = SystemMetaObject.forObject(stmtHandler);
+                    while(metaStamtHandler.hasGetter("h")){
+                        Object object = metaStamtHandler.getValue("h");
+                        metaStamtHandler = SystemMetaObject.forObject(stmtHandler);
+                    }
+                    while(metaStamtHandler.hasGetter("target")){
+                        Object object = metaStamtHandler.getValue("target");
+                        metaStamtHandler = SystemMetaObject.forObject(stmtHandler);
+                    }
+                    String sql = (String)metaStamtHandler.getValue("delegate.boundSql.sql");
+                    sql = sql.trim();
+                    String pageSql = sql+" limit #{firstIndex},#{pageSize}";
+                    return invocation.proceed();
+                }
+            }
         }
-
         return invocation.proceed();
     }
 
