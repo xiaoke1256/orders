e964f82fb451e2cb1e68a4e9b2ee3533ff943351
xiaoke_1256 <xiaoke_1256@sina.com>
2023-06-06 17:48:44 +0800

    安装es

--- docker-images/k8s/es.sh
+++ docker-images/k8s/es.sh
@@ -1,3 +1,3 @@
-docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -v esdata:/usr/share/elasticsearch/data -d --restart=always --network=host docker.elastic.co/elasticsearch/elasticsearch:7.2.0
+docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -v esdata:/usr/share/elasticsearch/data -v /home/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml -d --restart=always --network=host docker.elastic.co/elasticsearch/elasticsearch:7.2.0
 
 docker run -p 9100:9100 -d --restart=always mobz/elasticsearch-head:5-alpine
