9065a5903f098dc03cff354b9c6520e1f792f39e
xiaoke_1256 <xiaoke_1256@sina.com>
2023-06-08 19:46:00 +0800

    elk

--- docker-images/k8s/es.sh
+++ docker-images/k8s/es.sh
@@ -1,3 +1,6 @@
 docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -v esdata:/usr/share/elasticsearch/data -v /home/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml -d --restart=always --network=host docker.elastic.co/elasticsearch/elasticsearch:7.2.0
 
 docker run -p 9100:9100 -d --restart=always mobz/elasticsearch-head:5-alpine
+
+
+docker run -d -it --restart=always  --privileged=true  --name=logstash -p 5047:5047 -p 9600:9600 -v /home/elk/logstash/pipeline/:/usr/share/logstash/pipeline/  docker.elastic.co/logstash/logstash:7.7.1
