c875ba68a67238835d953a331e8a5d719a2f0e50
xiaoke_1256 <xiaoke_1256@sina.com>
2024-03-17 16:23:07 +0800

    Login 页面

--- orders-web-shop/orders-shop-ui/src/main.ts
+++ orders-web-shop/orders-shop-ui/src/main.ts
@@ -2,5 +2,15 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import viewUIPlus from 'view-ui-plus'
+import './assets/style/public.css';
+import './assets/style/login.css';
+import './assets/style/home.css';
+import './assets/style/style.css';
+import 'view-ui-plus/dist/styles/viewuiplus.css'
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App)
+    .use(store)
+    .use(router)
+    .use(viewUIPlus)
+    .mount("#app");
