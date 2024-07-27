4fb5db18ac6fd81dac824110bfd5e8a4a2438a1c
jun.tang6 <jun.tang6@gientech.com>
2024-07-27 14:50:01 +0800

    数据库主从复制

--- orders/sql/0.create_database.sql
+++ orders/sql/0.create_database.sql
@@ -11,6 +11,8 @@ GRANT REPLICATION SLAVE ON *.* TO 'slaveUser'@'127.0.0.1';
 FLUSH PRIVILEGES;
 SHOW MASTER STATUS;
 
+ALTER USER 'slaveUser'@'127.0.0.1' IDENTIFIED WITH mysql_native_password;
+
 -- 主库加锁
 FLUSH TABLES WITH READ LOCK;
 
@@ -21,9 +23,14 @@ FLUSH TABLES WITH READ LOCK;
 -- mysql -uroot -pPassw0rd@_ < /opt/all-20231226.sql
 
 -- # 登上从库执行
-CHANGE MASTER TO MASTER_HOST='127.0.0.1', MASTER_PORT=3306,
+CHANGE MASTER TO MASTER_HOST='localhost', MASTER_PORT=3306,
     MASTER_USER='slaveUser', MASTER_PASSWORD='xiaoke_1256',
-    MASTER_LOG_FILE='mysql-bin.000001', MASTER_LOG_POS=328;
+    MASTER_LOG_FILE='mysql-bin.000001', MASTER_LOG_POS=2426; -- 其中2426 是 show master status 中显示的。
+
+-- 需要设置为：
+-- slave_sql_verify_checksum=0
+
+-- 如有必要设置 max_allowed_packet =1073741824;
 
 start slave;
 show slave status ;
