beaf506e2f3b4833ef81d0c9e8bc3f6937ab0a2c
xiaoke_1256 <xiaoke_1256@sina.com>
2024-08-19 18:05:08 +0800

    大数据测试

--- bigData/src/main/java/com/xiaoke_1256/orders/SparkMain.scala
+++ bigData/src/main/java/com/xiaoke_1256/orders/SparkMain.scala
@@ -5,8 +5,11 @@ import org.apache.spark.api.java.JavaPairRDD
 import org.apache.spark.api.java.JavaRDD
 import org.apache.spark.api.java.JavaSparkContext
 import org.apache.spark.storage.StorageLevel
+
+import java.io.File
 import scala.Tuple2
 import java.util
+import scala.reflect.io.Directory
 
 object SparkMain{
   @throws[Exception]
@@ -26,19 +29,27 @@ object SparkMain{
 
     // 将每一行通过空格截取成新的RDD
     //val words = lines.flatMap[Array[String]]((s: String) => util.Arrays.asList(s.split(" "):_*))
-    val words = lines.flatMap((s: String) => util.Arrays.asList(s.split(" "):_*).iterator)
-
+    val words = lines
+      .map((s:String)=>s.replaceAll("[\\&|\\.|\\,|\\:|…|\\?|\\!|\\'|\\\"|\\)|\\(|\\]|\\[|\\~|\\>|\\<]"," ")) //去掉标点符号
+      .filter((s:String)=>(!s.isEmpty))//去掉空字符串
+      .map(s=>s.toLowerCase) //转成小写
+      .flatMap((s: String) => util.Arrays.asList(s.split(" "):_*).iterator)
 
 //    def function1 = (input: String)=> Seq[String] = {
 //      // some processing that produces multiple outputs
 //      Seq("output1", "output2", "output3")
 //    }
     // 将所有的文字组成键值对，并对不同的key进行计数
-    val pairs = words.mapToPair((s:String) => new Tuple2[String,Integer](s, 1))
+    val pairs = words.mapToPair((s:String) => new Tuple2[String,Integer](s, 1)).filter(!_._1.isEmpty)
     val counts = pairs.reduceByKey((a: Integer, b: Integer) => a + b)
     // 循环输出每一个字的出现次数
-    counts.foreach((s: Tuple2[String, Integer]) => System.out.println(s._1 + "," + s._2))
+    counts
+      .sortBy(_._2,false)//先排序
+      .foreach((s: Tuple2[String, Integer]) => System.out.println(s._1 + "," + s._2))
     // 持久化到内存和硬盘中，能够为后期新的程序方便读取
+    val directory = new Directory(new File(outputPath))
+    directory.deleteRecursively()
+
     counts.persist(StorageLevel.MEMORY_AND_DISK)
     // 输出成文本到指定目录
     counts.saveAsTextFile(outputPath)
