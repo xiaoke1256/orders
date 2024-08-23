87426a1f77551b4eca1d9bc4b73c97c8c74bde74
xiaoke_1256 <xiaoke_1256@sina.com>
2024-08-23 15:25:25 +0800

    Kmeans聚类

---
+++ bigData/src/main/java/com/xiaoke_1256/orders/bigdata/product/KmeanCluster.scala
@@ -0,0 +1,32 @@
+package com.xiaoke_1256.orders.bigdata.product
+
+import org.apache.spark.SparkConf
+import org.apache.spark.api.java.JavaSparkContext
+import org.apache.spark.mllib.clustering.KMeans
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.util.MLUtils
+
+
+/**
+ * Kmeans 聚类算法
+ * 针对商品，两个维度：价格，年销量
+ */
+object KmeansCluster {
+  val appName = "kmeansCluster"
+  // 设定spark master（默认支持local）
+  val master = "local"
+
+  val filePath = "D:\\test\\input\\KmeansCluster.txt";
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName(appName).setMaster(master)
+    val sc = new JavaSparkContext(conf)
+    val data = MLUtils.loadLibSVMFile(sc,filePath)
+    val parsedData = data.map( s => s.getFeatures).cache()
+    //
+    val numClusters = 10;//最大分类数
+    val numIterator = 20;//迭代次数
+    val model = KMeans.train(parsedData,numClusters,numIterator);
+    model.clusterCenters.foreach(println)
+  }
+}
