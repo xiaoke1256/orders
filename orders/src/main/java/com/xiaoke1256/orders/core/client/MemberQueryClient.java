b3600ce499c41eab2750fc751b993276a70c4747
xiaoke_1256 <xiaoke_1256@sina.com>
2024-08-25 22:28:38 +0800

    orders 重新运行起来

--- orders/src/main/java/com/xiaoke1256/orders/core/client/MemberQueryClient.java
+++ orders/src/main/java/com/xiaoke1256/orders/core/client/MemberQueryClient.java
@@ -7,10 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.xiaoke1256.orders.member.api.MemberQueryService;
 import com.xiaoke1256.orders.member.dto.Member;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(name="api-product",url="${remote.api.product.uri}",fallbackFactory=ProductQueryFallbackFactory.class,path="product")
 public interface MemberQueryClient extends MemberQueryService {
 	
 	@GetMapping("/members")
 	public List<Member> findAll();
+
+	@RequestMapping(value="/member/{accountNo}",method={RequestMethod.GET})
+	public Member getMember(String accountNo);
+
+
 }
--- orders/src/main/java/com/xiaoke1256/orders/core/controller/MemberQueryController.java
+++ orders/src/main/java/com/xiaoke1256/orders/core/controller/MemberQueryController.java
@@ -3,6 +3,7 @@ package com.xiaoke1256.orders.core.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +24,8 @@ public class MemberQueryController implements MemberQueryService {
 	}
 
 	@Override
-	public Member getMember(String accountNo) {
+	@RequestMapping(value="/member/{accountNo}",method={RequestMethod.GET})
+	public Member getMember(@PathVariable("accountNo") String accountNo) {
 		return null;
 	}
 
