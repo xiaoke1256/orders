7cd374092bac7216da2ffd9c78d56352f742069a
xiaoke_1256 <xiaoke_1256@sina.com>
2024-09-08 14:54:22 +0800

    保存预测结果——后端

--- bigData/sql/1.create_tables.sql
+++ bigData/sql/1.create_tables.sql
@@ -8,10 +8,19 @@ create table BIG_DATA_MODEL(
   algorithm_type VARCHAR(16) comment '模型算法：如Kmeans',
   train_param varchar(1024) comment '模型训练参数（包括维度系数）,封装成json',
   file_content BLOB comment '模型文件（二进制）',
-  class_define varchar(1024) comment '分类定义（聚集算法用），即分类的label和分类的名称的对应关系',
+  class_define varchar(1024) comment '分类定义（聚集算法用），即分类的label和分类的名称的对应关系（废除）',
   create_time DATETIME not null DEFAULT NOW() comment '创建时间',
   create_by VARCHAR(32) comment '创建人'
 )comment = '大数据模型' ENGINE=InnoDB CHARSET=utf8mb4;
+
+-- 聚类定义
+create table BIG_DATA_CLUSTER_DEFINED(
+    cluster_id BIGINT primary key auto_increment comment '主键',
+    model_id  BIGINT not null comment '模型主键',
+    label varchar(10) not null comment '（算法产生的）标签',
+    label_name varchar(128) comment '标签名',
+    insert_time DATETIME not null DEFAULT NOW() comment '创建时间'
+)comment = '聚类定义' ENGINE=InnoDB CHARSET=utf8mb4;
 
 
 -- 聚类算法的结果保存
@@ -22,21 +31,14 @@ create table BIG_DATA_CAL_EXEC_INFO(
     object_type char(2) comment '对象类型：01：商品，02：用户',
     algorithm_type VARCHAR(16) comment '执行算法',
     execute_time DATETIME not null DEFAULT NOW() comment '执行时间'
-);
--- 聚类
-create table BIG_DATA_CLUSTER(
-    cluster_id BIGINT primary key auto_increment comment '主键',
-    exec_id  BIGINT not null comment '执行主键',
-    label varchar(10) not null comment '（算法产生的）标签',
-    label_name varchar(128) comment '标签名',
-    insert_time DATETIME not null DEFAULT NOW() comment '创建时间'
-);
+)comment = '计算执行信息' ENGINE=InnoDB CHARSET=utf8mb4;
+
 -- 聚类与对象的映射
 create table BIG_DATA_CLUSTER_OBJECT_MAP(
    map_id BIGINT primary key auto_increment comment '主键',
    exec_id BIGINT not null comment '执行主键',
-   cluster_id BIGINT not null comment '聚类主键',
-   object_type char(2) comment '对象类型：01：商品，02：用户',
-   object_id BIGINT not null comment '对象主键',
+   label varchar(10) not null comment '（算法产生的）标签',
+   object_type char(2) comment '业务对象类型：01：商品，02：用户',
+   object_id varchar(64) not null comment '业务对象主键',
    insert_time DATETIME not null DEFAULT NOW() comment '创建时间'
-);
+)comment = '聚类与对象的映射' ENGINE=InnoDB CHARSET=utf8mb4;
