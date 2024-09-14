61b243e898b83c9a977b84eacae559832b2d69c8
xiaoke_1256 <xiaoke_1256@sina.com>
2024-09-14 11:07:57 +0800

    慢查询日志设置

--- orders/sql/0.slow_query_log_set.sql
+++ orders/sql/0.slow_query_log_set.sql
@@ -5,3 +5,9 @@ set GLOBAL slow_query_log=1
 set GLOBAL long_query_time=0.2
 
 SHOW VARIABLES LIKE 'slow_query_log_file';
+
+-- 或在配置文件中如下配置
+-- [mysqld]
+-- slow_query_log = 1
+-- slow_query_log_file = /var/lib/mysql/12912429fbd4-slow.log
+-- long_query_time = 0.2
