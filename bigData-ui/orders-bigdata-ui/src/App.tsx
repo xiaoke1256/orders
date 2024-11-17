d53d2cef093ebc05239ba84a9c9064ee090037dc
xiaoke_1256 <xiaoke_1256@sina.com>
2024-11-17 18:23:57 +0800

    + ts

--- bigData-ui/orders-bigdata-ui/src/App.tsx
+++ bigData-ui/orders-bigdata-ui/src/App.tsx
@@ -1,7 +1,8 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import MainRuoter from './MainRuoter';
 import {Link} from "react-router-dom";
+import MainRuoter from './MainRuoter';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
