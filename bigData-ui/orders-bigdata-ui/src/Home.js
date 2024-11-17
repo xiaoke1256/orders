29123b7652e6db2cf8d6c5c4a242d1d4ab08effc
xiaoke_1256 <xiaoke_1256@sina.com>
2024-11-17 11:01:49 +0800

    + antd

--- bigData-ui/orders-bigdata-ui/src/Home.js
+++ bigData-ui/orders-bigdata-ui/src/Home.js
@@ -1,7 +1,10 @@
+import { Button } from 'antd'
+
 export function Home(){
     return (
         <div> 
             This is Home Page.
+            <Button type="primary" > 按钮</Button>
         </div>
     );
 }
