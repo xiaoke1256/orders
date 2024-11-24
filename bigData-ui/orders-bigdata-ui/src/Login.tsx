40bd914fa25f124972cca60e8f5fe02bf0bd4554
xiaoke_1256 <xiaoke_1256@sina.com>
2024-11-24 13:11:51 +0800

    login page

--- bigData-ui/orders-bigdata-ui/src/Login.tsx
+++ bigData-ui/orders-bigdata-ui/src/Login.tsx
@@ -1,19 +1,23 @@
-import { Layout, Card, Flex  } from 'antd'
+import { Layout, Card, Flex  } from 'antd';
+import './common.css';
+import './Login.css'
+
 const { Header, Content, Footer } = Layout;
 
 export function Login(){
     return (
     <>
-      <Layout className="top h-full">
+      <Layout>
         <Header>
+          <div className="hearder-logo" >Orders</div>
         </Header>
         <Content className="loginContent">
-          <Flex justify={"flex-end"}>——欢迎访问商户端</Flex >
+          <Flex justify={"flex-end"} className='welcomeMsg' >——欢迎访问Orders大数据</Flex >
           <Card>
 
           </Card>
         </Content>
-        <Footer style={{ textAlign: 'center' }}>2021 &copy; xiaoke1256</Footer>
+        <Footer className='layout-footer-center'>2021 &copy; xiaoke1256</Footer>
       </Layout>
     </>
     )
