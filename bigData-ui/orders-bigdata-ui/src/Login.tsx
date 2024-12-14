4887cfb7aa445d740176102aeff06e0092822a10
xiaoke_1256 <xiaoke_1256@sina.com>
2024-12-14 23:17:07 +0800

    login page

--- bigData-ui/orders-bigdata-ui/src/Login.tsx
+++ bigData-ui/orders-bigdata-ui/src/Login.tsx
@@ -1,4 +1,4 @@
-import { Layout, Card, Flex, Image  } from 'antd';
+import { Layout, Card, Flex, Image, Form, Input, Button  } from 'antd';
 import './common.css';
 import './Login.css'
 
@@ -7,6 +7,8 @@ import './Login.css'
 const { Header, Content, Footer } = Layout;
 
 export function Login(){
+    const isScanLogin = false;
+
     return (
     <>
       <Layout>
@@ -16,8 +18,34 @@ export function Login(){
         <Content className="loginContent">
           <Flex justify={"flex-end"} className='welcomeMsg' >——欢迎访问Orders大数据</Flex >
           <Card>
-            <div>
-              <Image preview={false} src="/login.jpg" />
+            <div className="loginMain">
+              <div>
+                <Image preview={false} src="/login.jpg" />
+              </div>
+              <div style={{width:'320px'}}>
+                <Card>
+                  <div className="loginWelcome" style={{display:'inline-block',width:'100%'}} >
+                    <div style={{display:"inline-block",width:"49%"}}>请您登陆：</div>
+                    <div style={{display:"inline-block",width:"49%",textAlign:"right"}}>
+                    </div>
+                  </div>
+                  <Form >
+                    <Form.Item
+                      name="loginName"
+                      rules={[{ required: true, message: '用户名不能为空' }]}
+                    >
+                      <Input  placeholder="用户名"/>
+                    </Form.Item>
+                    <Form.Item
+                      name="password"
+                      rules={[{ required: true, message: '密码不能为空' }]}
+                    >
+                      <Input.Password  type="password" placeholder="密码" />
+                    </Form.Item>
+                    <Button type="primary" block onClick={()=>{}} >登录</Button>
+                  </Form>
+                </Card>
+              </div>
             </div>
           </Card>
         </Content>
