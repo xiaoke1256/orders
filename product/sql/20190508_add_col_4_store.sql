1d76a433003c8c3a16950ea8c96ead1eb6abc3c1
xiaoke_1256 <xiaoke_1256@sina.com>
2025-09-06 08:05:43 +0800

    Case Sensitive

--- product/sql/20190508_add_col_4_store.sql
+++ product/sql/20190508_add_col_4_store.sql
@@ -1,3 +1,3 @@
 -- 支付方式和支付账号
-alter table STORE add pay_type CHAR(3) null comment '支付方式，（目前全是3rdpay）';
-alter table STORE add pay_account_no varchar(18) null comment '支付账号';
+alter table store add pay_type CHAR(3) null comment '支付方式，（目前全是3rdpay）';
+alter table store add pay_account_no varchar(18) null comment '支付账号';
