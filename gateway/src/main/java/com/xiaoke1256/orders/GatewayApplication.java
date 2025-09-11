687248b3dca2a72337c8647c9564baea343cc122
xiaoke_1256 <xiaoke_1256@sina.com>
2025-09-11 12:51:02 +0800

    exclude = DataSourceAutoConfiguration

--- gateway/src/main/java/com/xiaoke1256/orders/GatewayApplication.java
+++ gateway/src/main/java/com/xiaoke1256/orders/GatewayApplication.java
@@ -2,9 +2,12 @@ package com.xiaoke1256.orders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages={"com.xiaoke1256.orders","org.springframework.http.codec"})
+@SpringBootApplication(
+        scanBasePackages={"com.xiaoke1256.orders","org.springframework.http.codec"},
+        exclude = DataSourceAutoConfiguration.class)
 public class GatewayApplication {
 
     public static void main(String[] args){
