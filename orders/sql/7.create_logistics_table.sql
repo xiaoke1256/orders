9b122357423319785007ae11882521228ca99ce3
xiaoke_1256 <xiaoke_1256@sina.com>
2025-11-13 16:40:39 +0800

    create table sql

--- orders/sql/7.create_logistics_table.sql
+++ orders/sql/7.create_logistics_table.sql
@@ -1,4 +1,4 @@
-create table LO_ORDER
+create table lo_order
 (
   lo_order_id BIGINT primary key not null auto_increment comment '物流单主键',
   lo_order_no VARCHAR(18) not null comment '物流单号',
@@ -9,4 +9,4 @@ create table LO_ORDER
   update_time TIMESTAMP not null DEFAULT NOW() comment '修改时间'
 ) comment='物流单表';
 
-CREATE UNIQUE INDEX IDX_COMPANY_LO_ORDER_NO ON LO_ORDER(company_code,lo_order_no);
+CREATE UNIQUE INDEX idx_company_lo_order_no ON lo_order(company_code,lo_order_no);
